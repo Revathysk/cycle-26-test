c726e0ac8d666a0a7f5e306f28fe1b29484567f8
Revathy <revathyskandasamy@gmail.com>
2021-11-17 15:38:40 -0600

    Conditionals IF and FOR loop excercise

---
+++ conditionals.js
@@ -0,0 +1,49 @@
+//define number positive or negative
+
+let integer=-1
+if (integer>0) 
+{
+    console.log("Positive :"+ integer)
+}
+else{console.log("Negative:"+ integer )}
+
+//task :Setting up age restriction
+const age=11
+if(age>=18)
+{console.log("Access granted...redirecting to website")}
+else (age<18)
+{ console.log("User should be 18 or above to access the website")}
+
+// Checking number is greater than or less than or negative
+
+//let num ='100';
+//if(num>100)
+//{console.log("Number is greater than 100")}
+//else if (num<100 && num>0)
+//{console.log("number is less than 100")}
+//else if (num<0)
+//{console.log("Number is Negative ")}
+//else if (num === 100)
+//{console.log(" Number is 100")}
+//else if (num === 0)
+//{console.log(" Number is Zero")}
+//else {console.log(" Error : It is a "+typeof(num))};
+//
+//Looping excercise
+// Loop variable cannot be a constant
+//for(let i=1;i<=10;++i)
+//{console.log(i);}
+//
+//for(let i=1;i<=10;i+=2)
+//{console.log(i);}
+
+//for(let i=0;i<=10;++i)
+//{console.log("Precedent:",i);}
+
+// for(let i=0;i<=10;i++)
+// {console.log("After:"+i);}
+
+//------- Remainder Operator---- //
+
+console.log(24%2 === 0)
+console.log(35%2 !== 0)
---
+++ loops.js
@@ -0,0 +1,18 @@
+//Looping excercise
+// Loop variable cannot be a constant
+//for(let i=1;i<=10;++i)
+//{console.log(i);}
+//
+//for(let i=1;i<=10;i+=2)
+//{console.log(i);}
+
+//for(let i=0;i<=10;++i)
+//{console.log("Precedent:",i);}
+
+// for(let i=0;i<=10;i++)
+// {console.log("After:"+i);}
+
+//------- Remainder Operator---- //
+
+console.log(24%2 === 0)
+console.log(35%2 !== 0)
